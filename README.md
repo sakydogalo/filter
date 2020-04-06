448078592de73626418f45bf78c3b419505001a9
Saky do Galo <sakydogalo@gmail.com>
2020-04-07 00:46:41 +0200

    added comments

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+!Filter
 process csv file without loss of data and with possibility to go back
--- prueba01.py
+++ prueba01.py
@@ -2,21 +2,24 @@
 # -*- coding: utf-8 -*-
 #
 #
+#
+#
 
 import csv
-
 
 with open('FACTURACION.csv',encoding='ISO-8859-1') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=';')
     line_count = 0
     lineas = []
-
     for row in csv_reader:
         if line_count == 0:
             campos = row
         else:
             lineas.append(row)
         line_count +=1
-    print('lineas:',line_count)
-    print(campos)
-    print(lineas)    
+    # print number of lines of the csv
+    print('Lineas:',line_count)
+    #print fields of the csv
+    print('Campos: ',campos)
+    #print the first row of the csv
+    print('Primera linea: ',lineas[0])
